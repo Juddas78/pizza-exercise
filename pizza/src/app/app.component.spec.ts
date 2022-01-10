3da8eba4f6e40e414bf949710bd3c78b98bf13c4
Judd Horton <judd.horton@gmail.com>
2022-01-09 21:58:08 -0600

    app component specs

--- pizza/src/app/app.component.spec.ts
+++ pizza/src/app/app.component.spec.ts
@@ -1,18 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { LoginScreenComponent } from './login-screen/login-screen.component';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
@@ -30,15 +41,64 @@ describe('AppComponent', () => {
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('#login')?.textContent).toContain('Please login to continue.');
   // });
-  it('should have a login screen', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const compiled = fixture.nativeElement as LoginScreenComponent;
-    const app = fixture.componentInstance;
-    app.loggedIn = false;
-    fixture.detectChanges();
-    expect(compiled).toBeTruthy();
+  describe('Not logged in user', () => {
+
+    beforeEach(()=> {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      app.loggedIn = false;
+      fixture.detectChanges();
+    })
+    it('should see login screen', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(fixture.debugElement.query(By.css('.dashboard'))).toBeNull();
+      expect(fixture.debugElement.query(By.css('.login'))).toBeTruthy();
+      expect(compiled.querySelector('.login')).toBeTruthy();
+    });
+
+    it('should not see the order page', () => {
+      fixture.componentInstance.logIn(false);
+      fixture.componentInstance.loggedIn = false;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('.dashboard'))).toBeNull();//need to get to the bottom of why these are the opposite of what I am expecting
+      expect(fixture.debugElement.query(By.css('.login'))).toBeTruthy();
+    });
+  })
+
+  describe('Logged in user', () => { 
+
+    beforeEach(()=> {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+
+    it('should not see login screen', () => {
+      fixture.componentInstance.logIn(true);
+      fixture.componentInstance.loggedIn = true;
+      fixture.detectChanges();
+
+      expect(fixture.debugElement.query(By.css('.dashboard'))).toBeTruthy();//need to get to the bottom of why these are the opposite of what I am expecting
+      expect(fixture.debugElement.query(By.css('.login'))).toBeNull();
+    });
+    it('should see order page', () => {
+      fixture.componentInstance.logIn(true);
+      fixture.componentInstance.loggedIn = true;
+      fixture.detectChanges();
+
+      expect(fixture.debugElement.query(By.css('.dashboard'))).toBeTruthy();//need to get to the bottom of why these are the opposite of what I am expecting
+      expect(fixture.debugElement.query(By.css('.login'))).toBeNull();
+      // fixture.detectChanges();
+      // expect(compiled).toBeTruthy();
+    });
   });
+
+  describe('logIn', () => {
+    it('user logs in', () => {
+      component.logIn(true);
+      expect(component.loggedIn).toBeTrue();
+    })
+  })
+
 });
